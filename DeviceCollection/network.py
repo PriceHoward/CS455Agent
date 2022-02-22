efea65f7bf56ec2f47f714f60ab2f965f4bcf226
Price Howard <pricechoward@gmail.com>
2022-02-21 20:34:24 -0600

    First commit of Prototype Agent.

---
+++ DeviceCollection/network.py
@@ -0,0 +1,35 @@
+import os.path
+import socket
+import json
+import DeviceCollection
+import sys
+import tqdm
+IPADDR = "localhost"
+PORT = 4483
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096
+fileName = "agent.json"
+fileSize = os.path.getsize(fileName)
+
+data = []
+s = socket.socket()
+s.connect((IPADDR, PORT))
+s.send(f"{fileName}{SEPARATOR}{fileSize}".encode())
+progress = tqdm.tqdm(range(fileSize), f"Sending {fileName}", unit="B", unit_scale=True, unit_divisor=1024)
+
+with open(fileName, "rb") as f:
+    while True:
+         # read the bytes from the file
+        bytes_read = f.read(BUFFER_SIZE)
+        if not bytes_read:
+            # file transmitting is done
+            break
+        # we use sendall to assure transimission in
+        # busy networks
+        s.sendall(bytes_read)
+        # update the progress bar
+        progress.update(len(bytes_read))
+# close the socket
+s.close()
+
+# Send the server the file size first. then try to send data.
---
+++ Json Files/README.txt
@@ -0,0 +1,5 @@
+What is in the json File?
+The first line is the CPU usage over 15 minutes of being used.
+The second line is the Disk usage split as: Total Disk, Used Disk, Free Disk, Percentage used.
+The third line is Memory Usage slit as: Total Memory, Availabale Memory, Percent Used, Used, Free, Active, Inactive, Wired.
+The fourth line is Network Usage split as: Bytes_Sent, Bytes_Recieved,Packets_Sent, Packets_Recieved, Errin, Errout, Dropin, Dropout
