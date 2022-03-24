899e9c9da89259b806347415ad8a6d43c9a92a9c
Price Howard <pricechoward@gmail.com>
2022-03-23 22:35:52 -0500

    Attempting to reformat JSON

--- DeviceCollection/DeviceCollection.py
+++ DeviceCollection/DeviceCollection.py
@@ -1,62 +1,54 @@
-import os
-import psutil
+from os import cpu_count
+from textwrap import indent
+from psutil import disk_usage, getloadavg, virtual_memory, net_io_counters
 import json
 import sys
 
 
-# Collects the total disk, used disk, and free disk.
-def diskusage():
-    return psutil.disk_usage("/")
-
-
 # Gets the CPU usage of the machine in the previous 15 minutes.
 def cpuusage():
-    load1, load5, load15 = psutil.getloadavg()
-    cpu_usage = (load15 / os.cpu_count()) * 100
+    load1, load5, load15 = getloadavg()
+    cpu_usage = (load15 / cpu_count()) * 100
     return cpu_usage
 
-
+# Collects the total disk, used disk, and free disk.
+def diskusage():
+    diskUsage = disk_usage("/")
+    return diskUsage[3]
 # Takes the memory usage such as used, free, active, total.
 def memoryusage():
-    return psutil.virtual_memory()
-
+    memory_usage = virtual_memory()
+    return memory_usage[2]
 
 # Return system-wide network I/O statistics as a named tuple.
 def networkusage():
-    return psutil.net_io_counters(pernic=False, nowrap=True)
-
+    network_usage = (net_io_counters().bytes_recv - net_io_counters().bytes_sent) / 100
+    return network_usage
 
 # Input everything into a file as a json.
 def fileCreation(cpuStats, diskStats, memoryStats, networkStats):
     with open('agent.json', 'w') as json_file:
-        json.dump(cpuStats, json_file)
+        json.dump({'CPU' : round(cpuStats)}, json_file)
         json_file.write("\n")
-        json.dump(diskStats, json_file)
+        json.dump({'DISK' : round(diskStats)}, json_file)
         json_file.write("\n")
-        json.dump(memoryStats, json_file)
+        json.dump({'MEMORY' : round(memoryStats)}, json_file)
         json_file.write("\n")
-        json.dump(networkStats, json_file)
+        json.dump({'NETWORK' : round(networkStats)}, json_file)
         json_file.close()
 
 
 def main():
-    #If statements are going to be used to see which system we are on.
-    # Will be useful if we need to create an executable.
-    system = sys.platform
-    if system == 'win32' or system == 'win64':
-        print("System is: " + system)
-        cpuStats = cpuusage()
-        diskStats = diskusage()
-        memoryStats = memoryusage()
-        networkStats = networkusage()
-        fileCreation(cpuStats, diskStats, memoryStats, networkStats)
-    elif system == 'Linux':
-        print("System is: " + system)
-        cpuStats = cpuusage()
-        diskStats = diskusage()
-        memoryStats = memoryusage()
-        networkStats = networkusage()
-        fileCreation(cpuStats, diskStats, memoryStats, networkStats)
+    cpuStats = cpuusage()
+    diskStats = diskusage()
+    memoryStats = memoryusage()
+    networkStats = networkusage()
+    fileCreation(cpuStats, diskStats, memoryStats, networkStats)
+    cpuStats = cpuusage()
+    diskStats = diskusage()
+    memoryStats = memoryusage()
+    networkStats = networkusage()
+    fileCreation(cpuStats, diskStats, memoryStats, networkStats)
 
 
 main()
