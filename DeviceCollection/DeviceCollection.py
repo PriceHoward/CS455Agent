d34977e8521e810a2c086fb5d3fa5c3039962944
Price Howard <pricechoward@gmail.com>
2022-04-04 21:43:58 -0500

    Added ability to send JSON data to server.

--- DeviceCollection/DeviceCollection.py
+++ DeviceCollection/DeviceCollection.py
@@ -1,7 +1,9 @@
 from os import cpu_count
-from psutil import disk_usage, getloadavg, virtual_memory, net_io_counters
+from psutil import disk_usage, getloadavg, virtual_memory, net_io_counters,cpu_percent
 import json
 from uuid import uuid4
+import time
+import network
 
 class DeviceCollection:
     def __init__(self):
@@ -13,8 +15,9 @@ class DeviceCollection:
     # Gets the CPU usage of the machine in the previous 15 minutes.
     def cpuusage(self):
         load1, load5, load15 = getloadavg()
-        self.cpu_usage = (load15 / cpu_count()) * 100
-        return self.cpu_usage
+        self.cpu_usage = (load1 / cpu_count())
+        return cpu_percent()
+
 
     # Collects the total disk, used disk, and free disk.
     def diskusage(self):
@@ -29,23 +32,27 @@ class DeviceCollection:
     # Return system-wide network I/O statistics as a named tuple.
     # TODO Rework the networkUsage to allow to find a percentage.
     def networkusage(self):
-        self.network_usage = (net_io_counters().bytes_recv - net_io_counters().bytes_sent) / 100000000
+        self.network_usage = (net_io_counters().bytes_recv/net_io_counters().bytes_sent)
         return self.network_usage
 
-
-    def jsonCreation(cpuStats, diskStats, memoryStats, networkStats):
+    def jsonCreation(cpuStats, diskStats, memoryStats, networkStats, customID):
             dataString = {
-                "ID": str(uuid4()),
-                "CPU": cpuStats,
-                "DISK": diskStats,
-                "MEMORY": memoryStats,
-                "NETWORK": networkStats
+                "ID": customID,
+                "CPU": round(cpuStats),
+                "DISK": round(diskStats),
+                "MEMORY": round(memoryStats),
+                "NETWORK": round(networkStats)
             }
-            print(json.dumps(dataString))
-
-        
+            jsonData = json.dumps(dataString)
+            print(jsonData)
+            network.ConnectToServerSend(jsonData)
+            
+    
 def main():
     device = DeviceCollection
-    device.jsonCreation(device.cpuusage(device), device.diskusage(device), device.memoryusage(device), device.networkusage(device))
-
+    customID = str(uuid4())
+    while(True):
+        device.jsonCreation(device.cpuusage(device), device.diskusage(device), device.memoryusage(device), device.networkusage(device), customID)
+        time.sleep(5)
+    
 main()
