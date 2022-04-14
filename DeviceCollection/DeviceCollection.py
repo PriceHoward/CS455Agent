28bd8f072bde8f029456d0983ced0cd1feb974ba
Price Howard <pricechoward@gmail.com>
2022-04-13 21:49:00 -0500

    New Executable Testing.

--- DeviceCollection/DeviceCollection.py
+++ DeviceCollection/DeviceCollection.py
@@ -3,8 +3,11 @@ from psutil import disk_usage, getloadavg, virtual_memory, net_io_counters,cpu_p
 import json
 from uuid import uuid4
 import time
-import network
+#import network
+import socket
 from datetime import datetime
+
+#from DeviceCollection.network import ConnectToServerSend
 
 class DeviceCollection:
     def __init__(self):
@@ -41,6 +44,14 @@ class DeviceCollection:
         current_time = now.strftime("%H:%M:%S")
         return current_time  
 
+    def ConnectToServerSend(data):
+        if data:
+            _host = "45.79.202.234"
+            _port = 8080
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((_host, _port))
+            sock.sendall(data.encode('utf-8'))
+
     def jsonCreation(cpuStats, diskStats, memoryStats, networkStats, customID, timeStamp):
             dataString = {
                 "ID": customID,
@@ -51,14 +62,22 @@ class DeviceCollection:
                 "NETWORK": round(networkStats)
             }
             jsonData = json.dumps(dataString)
-            print(jsonData)
-            network.ConnectToServerSend(jsonData)      
+            print(jsonData)      
+            return jsonData
+
+
+    
     
 def main():
     device = DeviceCollection
     customID = str(uuid4())
     while(True):
-        device.jsonCreation(device.cpuusage(device), device.diskusage(device), device.memoryusage(device), device.networkusage(device), customID, device.timestamp(device))
+        data = device.jsonCreation(device.cpuusage(device), device.diskusage(device), device.memoryusage(device), device.networkusage(device), customID, device.timestamp(device))
+        _host = "45.79.202.234"
+        _port = 8080
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((_host, _port))
+        sock.sendall(data.encode('utf-8'))
         time.sleep(5)
     
 main()
